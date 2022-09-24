871e5e549db514606c8b85785b6cd95ede2c8842
Michael Liem <michaelliem1999@gmail.com>
2022-09-24 16:48:20 +0700

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
-mkdir -p ~/.streamlit/echo "\
+mkdir -p ~/.streamlit
+
+echo "\
+[genera]\n\
+email = \"michaelliem1999@gmail.com\"\n\
+"> ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
-port = $PORT\n\
+enableXsrfProtection = false\n\
 enableCORS = false\n\
-\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
