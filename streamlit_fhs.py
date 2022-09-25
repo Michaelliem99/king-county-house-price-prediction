acb7eecfe20c1c4d08fd76227b473bd59b0e42ac
Michael Liem <michaelliem1999@gmail.com>
2022-09-26 02:09:08 +0700

    add ml model

--- streamlit_fhs.py
+++ streamlit_fhs.py
@@ -1,19 +1,35 @@
 import streamlit as st
 import joblib
 import pandas as pd
-
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 
 st.write("House Price Prediction")
 
+st.number_input("Enter the age of the house:", step=1)
 col1, col2 = st.columns(2)
 sqft_living = col1.number_input("Enter house living area in sqft", min_value=100.0)
 sqft_lot = col2.number_input("Enter house lot area in sqft", min_value=100.0)
-bedroom = col1.number_input("Enter number of bedrooms", max_value=15, step=1)
-bathroom = col2.number_input("Enter number of bathrooms", min_value=1.0, max_value=10.0, step=0.25)
+bedrooms = col1.number_input("Enter number of bedrooms", max_value=15, step=1)
+bathrooms = col2.number_input("Enter number of bathrooms", min_value=1.0, max_value=10.0, step=0.25)
 floor = col1.number_input("Enter number of floors", min_value=1.0, max_value=7.0, step=0.5)
 waterfront = col2.selectbox("Does the house has waterfront view?", ['Yes', 'No'])
-condition_index = col1.number_input("Enter house condition score", min_value=1, max_value=5, step=1)
-condition_index = col2.number_input("Enter house grade score", min_value=1, max_value=13, step=1)
+condition = col1.number_input("Enter house condition score", min_value=1, max_value=5, step=1)
+grade = col2.number_input("Enter house grade score", min_value=1, max_value=13, step=1
 
-st.button('Predict')
+df_pred = pd.DataFrame({
+    'sqft_living15':sqft_living, 'sqft_lot15':sqft_lot, 
+    'bedrooms':bedrooms, 'bathrooms':bathrooms, 
+    'floor':floor, 'waterfront':waterfront,
+    'condition':condition, 'grade':grade
+})
 
+waterfront_mapping = {'Yes':1, 'No':0}
+df_pred['waterfront'] = df_pred['waterfront'].replace(waterfront_mapping)
+    
+model = joblib.load('xgb_pipe.sav')
+prediction = model.predict(df_pred)
+
+if st.button('Predict'):
+    st.write('Your predicted house price is {}'.format(prediction))
